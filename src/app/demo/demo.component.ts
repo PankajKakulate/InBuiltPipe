5651d6becdbaaf22798a45b7e1d9a22347453e5e
pankaj <pankaj7187815@gmail.com>
2024-01-04 07:52:04 +0530

    i c 04-01-2024

---
+++ src/app/demo/demo.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './demo.component.html',
+  styleUrl: './demo.component.css'
+})
+export class DemoComponent {
+
+  public Name:string="marvellous infosystems pune";
+  public Value : number =78.647;
+  public Today =new Date();
+
+  public Book={
+    "Name" : "Learning Angular",
+    "Author" : "James Peter",
+    "Price" : 8000
+  }
+}
